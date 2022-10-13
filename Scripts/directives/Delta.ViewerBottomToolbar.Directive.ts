bead641ab088e87218eaf07330dc65ba4a4dac1c
Trung Vu <trungvu16222@gmail.com>
2022-10-13 09:02:59 +0700

    alo

---
+++ Scripts/directives/Delta.ViewerBottomToolbar.Directive.ts
@@ -0,0 +1,19 @@
+﻿directives.directive('bottomToolbar',
+    ["eventService", "$timeout", "queryArchiveService", "seriesManagerService",
+        "authenticationService", "dataService", "optionsService",
+        function (eventService: EventService, $timeout, queryArchiveService: QueryArchiveService,
+            seriesManagerService: SeriesManagerService,
+            authenticationService: AuthenticationService, dataService: DataService,
+            optionsService: OptionsService): ng.IDirective {
+            return {
+                replace: true,
+                template: '<div></div>',
+                scope: {
+                    retrieveUrl: '=',
+                    api: '=',
+                },
+                templateUrl: 'views/templates/AudioPlayer.html',
+            }
+        }
+    ]
+);
